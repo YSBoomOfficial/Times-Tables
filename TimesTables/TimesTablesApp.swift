0074478e24ab943536c1b9ed7bf539d44d547986
Yash <63893378+YSBoomOfficial@users.noreply.github.com>
2022-07-16 02:36:22 +0100

    Initial Commit

---
+++ TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by Yash Shah on 16/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
